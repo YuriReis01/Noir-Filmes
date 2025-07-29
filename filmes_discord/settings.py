a2af305a649c3608de157987a5596a0dbffa6220
YuriReis01 <yurireis116@gmail.com>
2025-07-28 21:04:23 -0300

    tmdb key

--- filmes_discord/settings.py
+++ filmes_discord/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+TMDB_API_KEY = os.environ.get('TMDB_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
